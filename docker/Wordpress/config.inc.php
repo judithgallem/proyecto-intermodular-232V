cbb7b60b7f68e67c6d1858383be0ddbcdb0dd46d
judithgallem <judith.gallem@gmail.com>
2026-05-11 10:31:34 +0200

    Add files via upload

---
+++ docker/Wordpress/config.inc.php
@@ -0,0 +1,12 @@
+/config.inc.php
+<?php
+
+$i = 0;
+
+$i++;
+
+$cfg['Servers'][$i]['host'] = 'wordpress-back';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
